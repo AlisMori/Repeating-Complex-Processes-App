a4e6706ada505f3ab2ced6e547d59b290cd2c183
eva-svetlana <eva86671@gmail.com>
2026-06-22 17:12:33 +0400

    Implemented task activity and tag APIs

--- templates_mgmt/urls.py
+++ templates_mgmt/urls.py
@@ -1,11 +1,19 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import TemplateViewSet
+from .views import (
+    TemplateViewSet,
+    TemplateTaskViewSet,
+    TemplateActivityViewSet,
+    TagViewSet,
+)
 
 
 router = DefaultRouter()
 router.register("templates", TemplateViewSet, basename="templates")
+router.register("template-tasks", TemplateTaskViewSet, basename="template-tasks")
+router.register("template-activities", TemplateActivityViewSet, basename="template-activities")
+router.register("tags", TagViewSet, basename="tags")
 
 urlpatterns = [
     path("", include(router.urls)),
--- templates_mgmt/views.py
+++ templates_mgmt/views.py
@@ -1,8 +1,13 @@
 from rest_framework import permissions, viewsets
 from rest_framework.filters import SearchFilter
 
-from .models import Template
-from .serializers import TemplateSerializer
+from .models import Template, TemplateTask, TemplateActivity, Tag
+from .serializers import (
+    TemplateSerializer,
+    TemplateTaskSerializer,
+    TemplateActivitySerializer,
+    TagSerializer,
+)
 
 
 class TemplateViewSet(viewsets.ModelViewSet):
@@ -12,9 +17,54 @@ class TemplateViewSet(viewsets.ModelViewSet):
     search_fields = ["template_name", "description"]
 
     def get_queryset(self):
-        # Users can see their own templates and public templates
+        # Users can see their own templates and public templates.
         return Template.objects.filter(user=self.request.user) | Template.objects.filter(is_public=True)
 
     def perform_create(self, serializer):
-        # Automatically attach the logged-in user to a new template
+        # The logged-in user automatically becomes the creator of the template.
         serializer.save(user=self.request.user, created_by_type="user")
+
+
+class TemplateTaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TemplateTaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [SearchFilter]
+    search_fields = ["task_name", "description", "note_text"]
+
+    def get_queryset(self):
+        # Users can only manage tasks that belong to their own templates or public templates.
+        return TemplateTask.objects.filter(
+            template__user=self.request.user
+        ) | TemplateTask.objects.filter(
+            template__is_public=True
+        )
+
+
+class TemplateActivityViewSet(viewsets.ModelViewSet):
+    serializer_class = TemplateActivitySerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [SearchFilter]
+    search_fields = ["activity_name", "description", "note_text"]
+
+    def get_queryset(self):
+        # Users can only manage activities that belong to their own templates or public templates.
+        return TemplateActivity.objects.filter(
+            template__user=self.request.user
+        ) | TemplateActivity.objects.filter(
+            template__is_public=True
+        )
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    serializer_class = TagSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [SearchFilter]
+    search_fields = ["tag_name"]
+
+    def get_queryset(self):
+        # Users only see their own tags.
+        return Tag.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        # The logged-in user automatically becomes the owner of the tag.
+        serializer.save(user=self.request.user)
