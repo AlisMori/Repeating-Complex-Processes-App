793160ef0c29c917625a0809cab6cb999eef9a96
Iskandar Chekayev <iskandarchekayev@trentu.ca>
2026-07-15 07:29:27 +0500

    Added modified apps.py

--- backend/notifications/apps.py
+++ backend/notifications/apps.py
@@ -2,4 +2,13 @@ from django.apps import AppConfig
 
 
 class NotificationsConfig(AppConfig):
-    name = 'notifications'
+
+    default_auto_field = "django.db.models.BigAutoField"
+
+    name = "notifications"
+
+    def ready(self):
+
+        from .scheduler import register_scheduler
+
+        register_scheduler()
